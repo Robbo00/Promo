3b81d11225f467f0e602f5a122bd21ef1e88e3c0
Robbo00 <rscrog606@west-mec.org>
2025-09-23 15:23:50 -0700

    tyui

--- 1/index.js
+++ 1/index.js
@@ -1,3 +1,12 @@
+let video = document.getElementById('vid')
+let cover = document.getElementById('dark')
+video.playbackRate = 0.9;
+
+setTimeout(function(){
+    cover.style.height = '100%'
+    video.playbackRate = 0.8;
+},2000)
+
 window.addEventListener('scroll', function (){
     if(window.scrollY >= 150){
         console.log('now')
