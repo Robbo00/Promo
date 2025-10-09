8354c8085dcb5892cbdd9fe5efb39cf61a755f04
Robbo00 <rscrog606@west-mec.org>
2025-10-09 15:12:13 -0700

    i

--- api.js
+++ api.js
@@ -6,44 +6,58 @@ import * as cheerio from "cheerio"
 
 const app = express()
 const PORT = 3000
-
-// Example route to scrape Beats products
+const { data } = await axios.get("https://www.beatsbydre.com/products", {headers: {'User-Agent': 'Mozilla/5.0'}})
+    const $ = cheerio.load(data)
+//route to scrape Beats products
 app.get("/api/beats", async (req, res) => {
   try {
-    const { data } = await axios.get("https://www.beatsbydre.com/products", {headers: {'User-Agent': 'Mozilla/5.0'}})
-    const $ = cheerio.load(data)
+    // Creates arrays for the information
     let names = []
     let price = []
     let imgo = []
     let products = []
+    // Goes through each card and gets the image src
     $(".product-slide").each((i, el) => {
-
       const img = $(el).find("img").attr("src")
       // const img = rawImg.startsWith("http") ? rawImg : `https://www.beatsbydre.com${rawImg}`
       imgo.push(img)
-
     })
-
+    // Goes through each header and gets the name of the products
     $('.product-detail__title--new').each((i, el) => {
       names.push($(el).find('span').text().trim())
     })
-
+    // Goes through and finds all the prices
     $('.product-detail__price').each((i, el) => {
       price.push($(el).find('span').text().trim())
     })
-
+    
+    //loops through and pushes the info to the poducts array
     for (let i = 0; i < names.length; i++) {
+      //pattern to determine type by reading the image path
       let pattern = /(headphones)|(speakers)|(earbuds)|(cables)|(cases)/
+      let triv = names[i].toLowerCase()
+      triv = triv.replaceAll(' ', '-')
+      let reg = new RegExp(triv)
+      let pImg = []
+      console.log(triv)
+      for (let i = 0; i < imgo.length; i++) {
+        if(imgo[i].match(reg)){
+          console.log('red')
+          pImg.push(imgo[i])
+          imgo.splice(i, 1)
+        }        
+      }
       let typ = imgo[i].match(pattern)
+      // Pushes to products
       products.push({
         name:names[i],
         price:price[i],
-        img:imgo[i],
+        img:pImg,
         type:typ[0]
       })
     }
-
-      res.json({n:names.length, i:imgo.length, p:price.length})
+    //Outputs products
+      res.json({products})
   } catch (err) {
     console.error(err.message)
     res.status(500).json({ error: "Failed to scrape data" })
